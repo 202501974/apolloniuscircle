bc5e9034f9e2841fac3df36791503538f05c1181
202501974 <gssd022@gmail.com>
2026-05-28 12:14:21 +0000

    아폴로니우스의 원 그리기 앱 제목 및 설명 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("아폴로니우스의 원 그리기")
+st.text_input("비율을 입력하세요", placeholder="예: 2:1")
+st.write("이곳에 비율을 입력하면 아폴로니우스의 원을 그릴 수 있습니다.")
